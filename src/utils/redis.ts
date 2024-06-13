22e0180cf32e228e9c1e088d4767e91a4a1f49ba
Russel Smith <russelsmith0125@hotmail.com>
2024-06-13 05:10:23 -0700

    redis setting fix

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -2,13 +2,14 @@ import { Redis } from "ioredis";
 import dotenv from "dotenv";
 import { env } from "node:process";
 dotenv.config();
-const REDIS_PORT = Number(env["REDIS_PORT"] ?? 6379);
-const REDIS_HOST = env["REDIS_HOST"] ?? "127.0.0.1";
+// const REDIS_PORT = Number(env["REDIS_PORT"] ?? 6379);
+// const REDIS_HOST = env["REDIS_HOST"] ?? "127.0.0.1";
+const REDIS_HOST = env["REDIS_URL"] ?? "127.0.0.1";
 
-console.log("REDIS_PORT", REDIS_PORT);
-console.log("REDIS_HOST", REDIS_HOST);
+// console.log("REDIS_PORT", REDIS_PORT);
+// console.log("REDIS_HOST", REDIS_HOST);
 
-const client = new Redis(REDIS_PORT, REDIS_HOST); // Connect to 127.0.0.1:6379 as default
+const client = new Redis(REDIS_HOST); // Connect to 127.0.0.1:6379 as default
 
 // If you want to connect to a different Redis server or port, you can do:
 // const client = new Redis({ host: 'some-host', port: some-port });
