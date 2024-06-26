3cbd919a943d98ed2df7bd88209239bfdd827238
Russel Smith <russelsmith0125@hotmail.com>
2024-06-26 01:41:28 -0700

    remaing temp2

--- src/server/handlers/sendData.ts
+++ src/server/handlers/sendData.ts
@@ -107,7 +107,7 @@ export async function sendRemainingClicks(id: string) {
     // Calculate the difference in milliseconds and convert to seconds
     const differenceInSeconds = (date2.getTime() - date1.getTime()) / 1000;
     const temp = remainingClicks - currentClicks + ( differenceInSeconds / 4 );
-    console.log("remaing temp", date2.getTime());
+    console.log("remaing temp", date2.getTime().toString());
     sendRemainingClicks = Math.min(temp, 1000);
   }
   console.log("Reaminig Clicks: ", sendRemainingClicks);
